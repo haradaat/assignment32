818e78f21b607f7aa70712be682732862fc35046
haradaat <78689186+haradaat@users.noreply.github.com>
2021-04-03 12:09:02 +0900

    Update janken.rb

--- janken.rb
+++ janken.rb
@@ -4,8 +4,8 @@ class Player
       puts "0:グー, 1:チョキ, 2:パー"
          input_hand = gets.to_i
        while true
-           if input_hand == 0 || 1 || 2
-             return input_hand
+           if input_hand == 0 || input_hand == 1 || input_hand == 2
+            return input_hand
            else
              puts "もう一度数字を入力してください。"
              puts "0:グー, 1:チョキ, 2:パー"
